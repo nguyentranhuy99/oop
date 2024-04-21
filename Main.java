219b708663eb8b20419cff989f8162a3cd0ce9e2
nguyentranhuy99 <166198920+nguyentranhuy99@users.noreply.github.com>
2024-04-21 15:28:52 +0700

    intro
    intro

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("Full Name: Nguyen Tran Huy"+'\t'+"Student Id: 23020378"+'\t'+"Class: K68AI2"+'\t'+"GitHub Account: nguyentranhuy99"+'\t'+"Email: 23020378@vnu.edu.vn");
+        for (int i=9;i>0;i--){
+            System.out.println(i+" bottles of beer on the wall, "+i+" bottles of beer.");
+            System.out.println("Take one down, pass it around");
+        }
+        System.out.println("No more bottles of beer on the wall.");
+    }
+}
