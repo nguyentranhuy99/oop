4661c9104fce0c19cfd8a7206bc6c53f3f2878e7
nguyentranhuy99 <166198920+nguyentranhuy99@users.noreply.github.com>
2024-04-22 08:21:04 +0700

    Update maxRun.java
    update

--- HW2/maxRun.java
+++ HW2/maxRun.java
@@ -1,15 +1,14 @@
 import java.util.Scanner;
 public class maxRun {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String s = sc.next();
+    public static int maxRun(String s){
         int max = 0;
         for (int i = 0; i < s.length() - 1; i++) {
             int c = 1;
             for (int j = i + 1; j < s.length(); j++) {
                 if (s.charAt(i) == s.charAt(j)) {
                     c++;
-                } else {
+                }
+                else {
                     if (c > max) {
                         max = c;
                     }
@@ -17,6 +16,11 @@ public class maxRun {
                 }
             }
         }
-        System.out.println(max);
+        return max;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        System.out.println(maxRun(s));
     }
 }
