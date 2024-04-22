1c2a3cbbe9056f295e96144f0a624399e3d2b669
nguyentranhuy99 <166198920+nguyentranhuy99@users.noreply.github.com>
2024-04-22 08:01:26 +0700

    add
    add

---
+++ HW2/String_blowup.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class String_blowup {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.next();
+        String s2="";
+        int c=0;
+        int d=0;
+        for (int i=0;i<s.length()-1;i++){
+            if(s.charAt(i)>='0'&&s.charAt(i)<='9') {
+                    int a = Integer.parseInt(String.valueOf(s.charAt(i)));
+                    String s1 = "";
+                    for (int j = 0; j < a; j++) {
+                        s1 = s1 + s.charAt(i + 1);
+                    }
+                    s2 = s2 + s.substring(c, i) + s1;
+                    c = i + 1;
+                    d = i;
+            }
+        }
+        for (int i=d+1;i<s.length()-1;i++){
+            s2=s2+s.charAt(i);
+        }
+        if(s.charAt(s.length()-1)<'0'||s.charAt(s.length()-1)>'9'){
+            s2=s2+s.charAt(s.length()-1);
+        }
+        System.out.println(s2);
+    }
+}
