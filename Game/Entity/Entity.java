5291d683087536e74c11b6de6f3b6c1dcfbc124e
Nguyễn Trần Huy <166198920+nguyentranhuy99@users.noreply.github.com>
2024-05-04 17:14:31 +0700

    Entity

---
+++ Game/Entity/Entity.java
@@ -0,0 +1,15 @@
+package Entity;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+public class Entity {
+    public int x,y;
+    public int speed;
+
+    public BufferedImage front1, front2, behide1, behide2, left1, left2, right1, right2;
+    public String direction;
+
+    public int spriteCount = 0;
+    public int spriteNum = 1;
+}
