625b1b008dffa9c95aac6d33a29d693a767013e2
Nguyễn Trần Huy <166198920+nguyentranhuy99@users.noreply.github.com>
2024-05-11 00:14:38 +0700

    Main update

--- Game/Main/Main.java
+++ Game/Main/Main.java
@@ -1,10 +1,11 @@
+package Main;
+
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.*;
 
 public class Main {
     public static void main(String[] args) {
-        long a = 20;
-        long b =30;
-        System.out.println(a/b);
+        new GameFrame();
     }
 }
